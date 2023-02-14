06cebc54ca5506d9cd473940df09d7618b16730a
tahayagizguler <yagizguler7@gmail.com>
2023-02-14 16:59:58 +0300

    commit for workflow

--- website/dynamodb.tf
+++ website/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "visiters" {
-  name_prefix           = var.dynamodb_table
+  name           = var.dynamodb_table
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
   write_capacity = 1
